e06877a2dc7ebf2941e3f33a1653927870ae5a6b
Zoe Eguaras <eguaraszoe@gmail.com>
2024-04-25 21:43:14 -0300

    Tratando de solucionar lo del Bin

---
+++ AyED/source/tp3/ejercicio2/RecorridosAG.java
@@ -0,0 +1,117 @@
+package tp3.ejercicio2;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import tp1.ejercicio8.Queue;
+import tp3.ejercicio1.GeneralTree;
+
+public class RecorridosAG {
+	private boolean esImpar (Integer num) {
+		boolean ok = true;
+		if (num % 2 == 0) {
+			ok = false;
+		}
+		return ok;
+	 }
+	
+	/* Método que retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
+	 pasados como parámetros, recorrido en preorden. */
+	
+	 public List<Integer> numerosImparesMayoresQuePreOrden (GeneralTree <Integer> a, Integer n) {
+		List <Integer> listaImpares = new LinkedList <Integer> ();
+		if ((a != null) && (! a.isEmpty())) {
+			numerosImparesMayoresQuePreOrdenHelper(a, n, listaImpares);
+		}
+		return listaImpares;
+	}
+	 
+	 private void numerosImparesMayoresQuePreOrdenHelper (GeneralTree <Integer> a, Integer n, List <Integer> lista) {
+		 if (esImpar(a.getData()) && a.getData() > n) {
+			 lista.add(a.getData());
+		 }
+		 List <GeneralTree <Integer>> children = a.getChildren();
+		 for (GeneralTree <Integer> child: children) {
+			 numerosImparesMayoresQuePreOrdenHelper(child, n, lista);
+		 }
+	 }
+	 
+	 /* Método que retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
+	 pasados como parámetros, recorrido en inorden. */
+	 
+	 public List<Integer> numerosImparesMayoresQueInOrden (GeneralTree <Integer> a, Integer n) {
+		 List <Integer> listaImpares = new LinkedList <Integer> ();
+			if ((a != null) && (! a.isEmpty())) {
+				numerosImparesMayoresQueInOrdenHelper(a, n, listaImpares);
+			}
+		 return listaImpares;
+	}
+	 
+	 private void numerosImparesMayoresQueInOrdenHelper (GeneralTree <Integer> a, Integer n, List <Integer> lista) {
+		 List <GeneralTree <Integer>> children = a.getChildren();
+		 Iterator <GeneralTree <Integer>> it = children.iterator();
+		 if (it.hasNext()) {
+			 GeneralTree <Integer> primerHijo = it.next();
+			 if (esImpar(primerHijo.getData()) && primerHijo.getData() > n) {
+				 lista.add(primerHijo.getData());
+			 }
+		 }
+		 if (esImpar(a.getData()) && a.getData() > n) {
+			 lista.add(a.getData());
+		 }
+		 if (it.hasNext()) {
+			 GeneralTree <Integer> child = it.next();
+			 numerosImparesMayoresQuePreOrdenHelper(child, n, lista);
+		 }
+	 }
+	 
+	 /* Método que retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
+	 pasados como parámetros, recorrido en postorden. */
+	 
+	 public List<Integer> numerosImparesMayoresQuePostOrden (GeneralTree <Integer> a, Integer n) {
+		 List <Integer> listaImpares = new LinkedList <Integer> ();
+			if ((a != null) && (! a.isEmpty())) {
+				numerosImparesMayoresQuePostOrdenHelper(a, n, listaImpares);
+			}
+		 return listaImpares;
+	}
+	 
+	 private void numerosImparesMayoresQuePostOrdenHelper (GeneralTree <Integer> a, Integer n, List <Integer> lista) {
+		 List <GeneralTree <Integer>> children = a.getChildren();
+		 for (GeneralTree <Integer> child: children) {
+			 numerosImparesMayoresQuePostOrdenHelper(child, n, lista);
+		 }
+		 if (esImpar(a.getData()) && a.getData() > n) {
+			 lista.add(a.getData());
+		 }
+	 }
+	 
+	 /* Método que retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
+	 pasados como parámetros, recorrido por niveles. */
+	 
+	 public List<Integer> numerosImparesMayoresQuePorNiveles(GeneralTree <Integer> a, Integer n) {
+		 List <Integer> listaImpares = new LinkedList <Integer> ();
+			if ((a != null) && (! a.isEmpty())) {
+				numerosImparesMayoresQuePorNivelesHelper(a, n, listaImpares);
+			}
+		 return listaImpares;
+	}
+	 
+	 private void numerosImparesMayoresQuePorNivelesHelper (GeneralTree <Integer> a, Integer n, List <Integer> lista) {
+		 GeneralTree<Integer> tree;
+		 Queue <GeneralTree<Integer>> cola = new Queue <GeneralTree<Integer>> ();
+		 cola.enqueue(a);
+		 
+		 while (!cola.isEmpty()) {
+			 tree = cola.dequeue();
+			 if (esImpar(tree.getData()) && tree.getData() > n) {
+				 lista.add(tree.getData());
+			 }
+			 List <GeneralTree <Integer>> children = tree.getChildren();
+			 for (GeneralTree <Integer> child: children) {
+				 cola.enqueue(child);
+			 }
+		 }
+	 }
+}
